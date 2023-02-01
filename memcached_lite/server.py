9ad72ff47b61583d114d99a3d553e01699278052
Asher Surber <risurb@iu.edu>
2023-01-31 20:22:59 -0500

    basic server and client

--- memcached_lite/server.py
+++ memcached_lite/server.py
@@ -1,15 +1,45 @@
 import os,sys,time
 from socket import *
 
-PORT = 9889
-HOST = "127.0.0.1"
-sock = socket.socket(AF_INET, SOCK_STREAM)
+if __name__ == '__main__':
+    PORT = 9889
+    HOST = "127.0.0.1"
+    sock = socket(AF_INET, SOCK_STREAM)
 
-sock.bind(HOST, PORT)
+sock.bind((HOST, PORT))
 sock.listen()
-print("Listening on port " + PORT)
+print("Listening on port " + str(PORT))
+
+def recvall(sock):
+    """
+    Function to receive data from the given socket
+    :param sock: connection of client
+    :return: data sent by the client
+    """
+
+    BUFF_SIZE = 10  # 1 KiB
+    data = b''
+    while True:
+        chunk = sock.recv(BUFF_SIZE)
+        data += chunk
+        if len(chunk) < BUFF_SIZE:
+            # either 0 or end of data
+            break
+    return data
+
 
 while True:
     clientSock, clientAddr = sock.accept()
-    print("Connected to {clientAddr}")
+    print("Connected to " + str(clientAddr))
     
+    # Receive data from Client
+    # Can we do better instead of hard coding it to 1024 bytes??
+    data = clientSock.recv(1024)
+
+    # data = recvall(conn)
+
+    # Send data back to client
+    clientSock.sendall(f"return data for id : {data.decode()}".encode())
+
+    # Print the received data
+    print(f"Address {clientAddr} \n Data: {data.decode()}")
